e32fad902ca6f1f8007a66e4a6fe1d81adb6d820
Paul Rondeau <paulrondeau07@gmail.com>
2013-05-04 21:30:27 -0400

    adding more foursquare functionality

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
+
+
 // Foursquare call used to grab venues in location
-$.get("https://api.foursquare.com/v2/venues/search?near=Toronto, ON&intent=browse&radius=1500&categoryId= 4d4b7105d754a06376d81259&oauth_token=JUPFSHFSPFGFUDZF4PWAWXFDRNK4FYSWYXU5GHQYVAAXHID1&v=20130504")
+var places = $.get("https://api.foursquare.com/v2/venues/search?near=Toronto, ON&intent=browse&radius=500&categoryId=4bf58dd8d48988d11f941735,4bf58dd8d48988d116941735,4bf58dd8d48988d11b941735,4bf58dd8d48988d11d941735,4bf58dd8d48988d120941735&oauth_token=JUPFSHFSPFGFUDZF4PWAWXFDRNK4FYSWYXU5GHQYVAAXHID1&v=20130504")
+var placeNames = place.responseText
