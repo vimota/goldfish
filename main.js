8ce3d856773c979b33e2045ceccad30dfb5035f2
Greg Kerr <greg.m.kerr@gmail.com>
2013-05-04 23:30:40 -0400

    button calls paul's function to populate VENUES global

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
   var TWITTER_USERNAME = document.getElementById("usrName").value,
       TWITTER_CONSUMER_KEY = "rdyanWJSlClGZyUOZFsbrg",
       TWITTER_CONSUMER_SECRET = "j5eBd36HKo96GkFF9NX7qUa34cERVTwYLuAR40lhRI",
-	  LOC = document.getElementById("usrLoc").value;
+	  LOC = document.getElementById("usrLoc").value,
+	  VENUES;
 
   var cb = new Codebird;
   cb.setConsumerKey(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET);
@@ -11,7 +12,7 @@ $(document).ready(function() {
   $.get("https://api.foursquare.com/v2/venues/search?near=Toronto, ON&intent=browse&radius=1500&categoryId= 4d4b7105d754a06376d81259&oauth_token=JUPFSHFSPFGFUDZF4PWAWXFDRNK4FYSWYXU5GHQYVAAXHID1&v=20130504")
 
   function btnConnectClick(){
-	alert("btnConnectClick");
+	VENUES = getVenueNames(LOC);
   }
 
   function getRelatedTweets(username, venues, isPublic) {
