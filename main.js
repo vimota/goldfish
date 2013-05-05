bd49bc3bc3e3f227d534e71a4addc9cfae6bf129
Paul Rondeau <paulrondeau07@gmail.com>
2013-05-04 20:38:35 -0400

    added foursquare call

---
+++ main.js
@@ -0,0 +1,2 @@
+// Foursquare call used to grab venues in location
+$.get("https://api.foursquare.com/v2/venues/search?near=Toronto, ON&intent=browse&radius=1500&categoryId= 4d4b7105d754a06376d81259&oauth_token=JUPFSHFSPFGFUDZF4PWAWXFDRNK4FYSWYXU5GHQYVAAXHID1&v=20130504")
