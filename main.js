aae02780c58341080c3ddabb43db2e1e4a0889e1
Victor Mota <vimota@gmail.com>
2013-05-04 20:42:40 -0400

    goodu?

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
+var TWITTER_USERNAME = "vimota";
+
+
 // Foursquare call used to grab venues in location
 $.get("https://api.foursquare.com/v2/venues/search?near=Toronto, ON&intent=browse&radius=1500&categoryId= 4d4b7105d754a06376d81259&oauth_token=JUPFSHFSPFGFUDZF4PWAWXFDRNK4FYSWYXU5GHQYVAAXHID1&v=20130504")
+
+
+
+function getRelatedTweets(username, venues) {
+  return [{}];
+}
+
