d6e401f686e7b1d968dfbd4d30c30d28b1c82015
Greg Kerr <greg.m.kerr@gmail.com>
2013-05-04 22:23:05 -0400

    changed layout, added event handlers

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 // Foursquare call used to grab venues in location
 $.get("https://api.foursquare.com/v2/venues/search?near=Toronto, ON&intent=browse&radius=1500&categoryId= 4d4b7105d754a06376d81259&oauth_token=JUPFSHFSPFGFUDZF4PWAWXFDRNK4FYSWYXU5GHQYVAAXHID1&v=20130504")
+
+function btnConnectClick(){
+	var usr = document.getElementById("usrName").value;
+	var loc = document.getElementById("usrLoc").value;
+	alert("btnConnectClick" + " : " + usr + " : " + loc);
+	callFourSquare();
+	doTwitterStuff();
+}
+
+function callFoursquare(){
+	
+}
+
+function doTwitterStuff(){
+	
+}
